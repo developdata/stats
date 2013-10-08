b0de8c7f80fd8a2f0eaa1672196115339afd074a
Indie <indira@developfor.com>
2013-10-08 12:34:53 +0100

    Add a welcome controller and view

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Stat::Application.config.secret_key_base = 'cf3946a288e2740a8d2d9e4d7f4eb8d6a31f460dc7247856d148e3e645af420b30929e6103d1a7b36ad052f963fb1cc36a339a3efd4318e6f3424ea06cd2fed7'
